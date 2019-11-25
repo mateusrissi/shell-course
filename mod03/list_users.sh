8f3f3fecd6bb5f93cd74bd3f983d373b7d94ca55
Mateus Rissi <mtrissi@gmail.com>
2019-11-25 14:01:19 -0300

    adding shift in while loop

--- mod03/list_users.sh
+++ mod03/list_users.sh
@@ -17,9 +17,13 @@
 # ------------------------------------------------------------------------ #
 # History:
 #
-#   v1.0 22/11/2019, Mateus:
+#   v1.0.0 22/11/2019, Mateus:
 #       - Start
 #       - Funcionalities
+#
+#   v1.1.0 22/11/2019, Mateus:
+#       - adding shift in while loop
+#       - adding flags
 # ------------------------------------------------------------------------ #
 # Tested on:
 #   bash 4.4.20
@@ -36,15 +40,26 @@ HELP_MESSAGE="
     -u - Convert output to uppercase
 "
 
-VERSION="v1.0.0"
+VERSION="v1.1.0"
+SORT_KEY=0
+UPPER_KEY=0
 # ------------------------------------------------------------------------ #
 
 # ------------------------------- EXEC ----------------------------------- #
-case "${1}" in
-    -h) echo "${HELP_MESSAGE}" && exit 0 ;;
-    -v) echo "${VERSION}" && exit 0      ;;
-    -s) echo "${USERS}" | sort && exit 0 ;;
-    -u) echo "${USERS^^}" && exit 0      ;;
-     *) echo "${USERS}" && exit 0        ;;
-esac
+while test -n "${1}"
+do
+    case "${1}" in
+        -h) echo "${HELP_MESSAGE}" && exit 0                           ;;
+        -v) echo "${VERSION}" && exit 0                                ;;
+        -s) SORT_KEY=1                                                 ;;
+        -u) UPPER_KEY=1                                                ;;
+         *) echo "Invalid option '${1}'! Use '-h' for help." && exit 1 ;;
+    esac
+    shift
+done
 # ------------------------------------------------------------------------ #
+
+[[ SORT_KEY -eq 1 ]] && USERS=$(echo "${USERS}" | sort)
+[[ UPPER_KEY -eq 1 ]] && USERS=$(echo "${USERS^^}")
+
+echo "${USERS}"

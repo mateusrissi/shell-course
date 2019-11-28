213da3674f6b459c924f48214b4c8ca65632be75
Mateus Rissi <mtrissi@gmail.com>
2019-11-28 09:46:58 -0300

    still not working

--- mod03/mod03_exercise01.sh
+++ mod03/mod03_exercise01.sh
@@ -10,7 +10,7 @@
 #  Organize your files in folders according to their extensions.
 #
 #  Examples:
-#      $ ./file_organizer.sh directory_path
+#      $ ./file_organizer.sh /path/to/dir
 #      Command description.
 #
 # --------------------------------------------------------------------------- #
@@ -27,28 +27,41 @@
 # --------------------------------------------------------------------------- #
 
 # -------------------------------- VARIABLES -------------------------------- #
-FOLDERS='Compressed Documents Images Music Videos Miscellaneous'
+FOLDERS="Compressed Documents Images Music Videos"
+
 declare -A extensionsDic=(
-    ['Compressed']='*.7z *.arj *.deb *.pkg *.rar *.rpm *.tar.gz *.zip *.z'
-    ['Documents']='*.ppt *.pptx *.odp *.xls *.xlsx *.ods *.doc *.docx \
-                   *.odt *.pdf *.rtf *.txt'
-    ['Images']='*.psd *.xcf *.tif *.tiff *.bmp *.jpg *.jpeg *.gif *.png'
-    ['Music']='*.wav *.mp3 *.ogg *.gsm *.dct *.flac *.au *.aiff *.vox'
-    ['Videos']='*.avi *.flv *.wmv *.mp4 *.mov'
-    ['Miscellaneous']=''
+    ["Compressed"]="*.7z *.arj *.deb *.pkg *.rar *.rpm *.tar.gz *.zip *.z"
+    ["Documents"]="*.ppt *.pptx *.odp *.xls *.xlsx *.ods *.doc *.docx \
+                   *.odt *.pdf *.rtf *.txt"
+    ["Images"]="*.psd *.xcf *.tif *.tiff *.bmp *.jpg *.jpeg *.gif *.png"
+    ["Music"]="*.wav *.mp3 *.ogg *.gsm *.dct *.flac *.au *.aiff *.vox"
+    ["Videos"]="*.avi *.flv *.wmv *.mp4 *.mov"
 )
+
+DIR="${1}"
+
 # ---------------------------------- TESTS ---------------------------------- #
-if [[ $(ls ${1} | wc -l) -eq 0 ]]; then
+if [[ $(ls ${DIR} | wc -l) -eq 0 ]]; then
     echo "There are no files in the specified folder" && exit 0
 fi
 
 # -------------------------------- FUNCTIONS -------------------------------- #
-organizer()={
+organizer() {
+    for i in ${FOLDERS[@]}; do
+        for line in `ls ${DIR} | sed '/extensionsDic[${i}]/p'`; do
+            mv ${DIR}${line} "${DIR}${i}/"
+        done
+    done
+}
 
+main() {
+    for i in ${FOLDERS[@]}; do
+        [[ -d "${DIR}${i}" ]] || mkdir "${DIR}${i}"
+    done
+    organizer
+    echo "Successfully organized the directory!"
 }
 
 # ----------------------------------- EXEC ---------------------------------- #
 
-organizer
-
-echo "Successfully organized directory!"
+main

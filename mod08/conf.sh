e1bc8eee99f9c9b28130d1cfb93504e1b766bc7d
Mateus Rissi <mtrissi@gmail.com>
2019-12-13 14:00:12 -0300

    added module 08 - configuration files

--- mod08/conf.sh
+++ mod08/conf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# script_name.sh - Brief description
+# conf.sh - Brief description
 
 # Site:       https://www.linkedin.com/in/mateusrissi/
 # Author:     Mateus Rissi
@@ -8,29 +8,30 @@
 
 #  Full description
 #  Examples:
-#      $ ./script_name.sh -d 1
+#      $ ./conf.sh
 #      Command description.
 
 # History:
-#   v1.0.0 03/10/2019, Mateus:
+#   v1.0.0 13/12/2019, Mateus:
 #       - Start
 #       - Funcionalities
-#   v1.1.0 10/10/2019, Mateus:
-#       - Modified functions bip bop
 
 # Tested on:
 #   bash 4.4.20
-
-# Thanks:
-# 	Bip - Found a bug at the login snippet.
-#	Bop - Suggested the addition of -h at the script calling.
 # --------------------------------------------------------------------------- #
 
 # VARIABLES
+CONF_FILE="config.cf"
+COLOR_ON="1"
+UPPERCASE_ON="1"
+MESSAGE="Test message"
 
+PURPLE="\033[35;1m"
+CYAN="\033[36;1m"
+NO_COLOR="\033[0m"
 
 # TESTS
-
+[[ ! -r ${CONF_FILE} ]] && echo "You do not have the permission to read the configuration file!" && exit 1
 
 # FUNCTIONS
 
